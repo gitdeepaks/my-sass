d22b35d52b918b2d1a641581205d8b6ccaa7c8e2
gitdeepaks <deepaksankhyan92@outlook.com>
2024-02-16 21:08:10 +0530

    first commit🚀

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <p className="text-6xl text-green-500">Hello AI Sass</p>
+      <Button variant="secondary">Hello Button</Button>
+    </>
+  );
+}
